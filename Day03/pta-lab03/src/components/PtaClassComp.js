1e0223a2aba41150c2cb170ecb0c35a6ff6d7bfb
unknown <sktkgame69@gmail.com>
2025-02-17 15:11:50 +0700

    Day04

---
+++ Day03/pta-lab03/src/components/PtaClassComp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class PtaClassComp extends Component {
+    render() {
+        return (
+            <div>
+                <h1> Đây là class components</h1>
+                <p>K23CNT3 - ReactJS class components</p>
+            </div>
+        )
+    }
+}
---
+++ Day03/pta-lab03/src/components/PtaFuncComp.js
@@ -0,0 +1,12 @@
+
+function PtaFuncComp() {
+    return (
+        <div>
+            <h1>Đây là Funcion Components</h1>
+            <p> Phạm Tuấn Anh </p>
+
+        </div>
+    );
+}
+
+export default PtaFuncComp; 
