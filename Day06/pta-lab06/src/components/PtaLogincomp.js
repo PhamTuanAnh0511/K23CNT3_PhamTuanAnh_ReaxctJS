a5656e7f2871d67e67ed8be188698d7146f64d70
PhamTuanAnh0511 <sktkgame69@gmail.com>
2025-03-19 20:32:22 +0700

    Delete Day06/pta-lab06 directory

--- Day06/pta-lab06/src/components/PtaLogincomp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function PtaLogincomp() {
-  return (
-    <div>
-       <h2> Welcome to my Applicatio</h2>
-    </div>
-  )
-}
--- Day06/pta-lab06/src/components/PtaLogoutCom.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function PtaLogoutCom() {
-  return (
-    <div>
-        <h2> please sign in</h2>
-    </div>
-  )
-}
